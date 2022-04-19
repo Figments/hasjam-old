224b09530559df15276ced39413807a1a5aa34e7
adi-dev-1337 <adi.dev.1337@gmail.com>
2022-04-19 01:58:18 +0100

    Drip improve

--- src/lib/ui/nav/Nav.svelte
+++ src/lib/ui/nav/Nav.svelte
@@ -12,4 +12,61 @@
 		  	font-family: var(--ui-text);
 		}
 	}
+.has {
+	-webkit-text-stroke-color: $drip-white;
+	-webkit-text-fill-color: $drip-white;
+}
+.ticker-container {
+	position: fixed;
+	width: 100%;
+	bottom: 0px;
+	font-size: 4rem;
+	font-style: bold;
+	overflow: hidden;
+	border: 1rem solid $drip-red;
+	height: 8rem;
+	background: $drip-grey;
+	-webkit-text-stroke-width: 2px;
+	-webkit-text-stroke-color: $drip-red;
+	-webkit-text-fill-color: $drip-red;
+	z-index: 100;
+	.ticker {
+		width: 100%;
+		white-space: nowrap;
+		display: block;
+        animation: moving-left 15s linear infinite;	
+		z-index: 100;
+		div {
+			display: inline-block;
+			margin: 0 5rem;
+		}
+	}
+}
+@keyframes moving-left {
+	0% {
+		transform: translate3d(0%, 0, 0);
+	}
+	100% {
+		transform: translate3d(-50%, 0, 0);
+	}
+}
+
 </style>
+
+<div class="ticker-container">
+	<div class="ticker">
+		<div><span class="has">has</span>JAM</div>
+		<div><span class="has">has</span>JAM</div>
+		<div><span class="has">has</span>JAM</div>
+		<div><span class="has">has</span>JAM</div>
+		<div><span class="has">has</span>JAM</div>
+		<div><span class="has">has</span>JAM</div>
+		<div><span class="has">has</span>JAM</div>
+		<div><span class="has">has</span>JAM</div>
+		<div><span class="has">has</span>JAM</div>
+		<div><span class="has">has</span>JAM</div>
+		<div><span class="has">has</span>JAM</div>
+		
+		
+	</div>
+</div>
